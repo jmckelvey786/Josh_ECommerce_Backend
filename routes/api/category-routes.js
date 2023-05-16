feaecc9391412a2671f6da1d45f25572e5b1ac06
jmckelvey786 <77301869+jmckelvey786@users.noreply.github.com>
2023-05-15 22:10:12 -0400

    added routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,77 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try{
+    const data = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try{
+    const data = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    if (!data) {
+      res.status(404).json({ message: 'Category Not found' });
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+   try {
+    const newCategory = req.body;
+    const category = await Category.create(newCategory);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async(req, res) => {
+   try {
+    const data = await Category.update({
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+    })
+    if(!data[0]) {
+      res.status(404).json({message: 'ID not Found'})
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async(req, res) => {
+   try{
+    const data = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!data) {
+      res.status(404).json({ message: 'No category found' });
+      return;
+    }
+    res.status(200).json({ message: 'Category deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+   try {
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+    if (!product) {
+      res.status(404).json({ message: "There is no product with that id" });
+      return;
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -89,8 +105,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const data = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!data) {
+      res.status(404).json({ message: 'Product ID not found' });
+      return;
+    }
+    res.status(200).json({ message: 'Product Successfully deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
